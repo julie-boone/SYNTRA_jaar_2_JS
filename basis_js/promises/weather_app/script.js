ef979bbd583ef0ebfb8d06677015ab789b02ef55
Julie <julieboone@live.be>
2023-11-27 18:45:10 +0100

    oefeningen

--- basis_js/promises/weather_app/script.js
+++ basis_js/promises/weather_app/script.js
@@ -24,9 +24,18 @@ const BASE_URL_GEOLOC =
   "http://api.openweathermap.org/geo/1.0/direct?limit=1&appid=33dcc2f20d92c833f0a2b35480e37b1f&q=";
 
 const BASE_URL_WEATHER =
-  "https://api.openweathermap.org/data/2.5/weather?appid=33dcc2f20d92c833f0a2b35480e37b1f";
+  "https://api.openweathermap.org/data/2.5/weather?appid=33dcc2f20d92c833f0a2b35480e37b1f&units=metric";
+
+const BASE_URL_FORECAST =
+  "https://api.openweathermap.org/data/2.5/forecast?appid=33dcc2f20d92c833f0a2b35480e37b1f&units=metric";
 
 function showWeather() {
+  const currentWeatherField = document.querySelector("#currentWeatherField");
+  const forecastField = document.querySelector("#forecastField");
+  if (currentWeatherField) {
+    currentWeatherField.remove();
+    forecastField.remove();
+  }
   locationToCoordinates()
     .then((coor) => Promise.all([getCurrentWeather(coor), getForecast(coor)]))
     .then(printWeather);
@@ -47,6 +56,7 @@ function getCurrentWeather({ lat, lon }) {
     .then((res) => res.json())
     .then((res) => {
       return {
+        name: res.name,
         main: res.main,
         weather: res.weather[0],
         wind: res.wind,
@@ -55,32 +65,103 @@ function getCurrentWeather({ lat, lon }) {
 }
 
 function getForecast({ lat, lon }) {
-  return fetch(`${BASE_URL_WEATHER}&lat=${lat}&lon=${lon}`)
+  return fetch(`${BASE_URL_FORECAST}&lat=${lat}&lon=${lon}`)
     .then((res) => res.json())
     .then((res) => {
+      console.log(res);
       return {
-        main: res.main,
-        weather: res.weather[0],
-        wind: res.wind,
+        name: res.city.name,
+        list: res.list,
       };
     });
 }
 
 function printWeather(res) {
   console.log(res);
+  printCurrentWeather(res);
+  printForecast(res);
+}
+
+function printCurrentWeather(res) {
   const body = document.querySelector("body");
-  const weatherField = document.createElement("div");
-  body.appendChild(weatherField);
-  const rainField = document.createElement("li");
-  rainField.classList.add("rain");
-  weatherField.appendChild(rainField);
-  const tempField = document.createElement("li");
-  tempField.classList.add("temp");
-  weatherField.appendChild(tempField);
-  const windField = document.createElement("li");
-  windField.classList.add("wind");
-  weatherField.appendChild(windField);
-  rainField.innerText = `Weather today: ${res[0].weather.description}`;
-  tempField.innerText = `Temperature: ${res[0].main.temp - 272.15} °C`;
-  windField.innerText = `Wind: ${res[0].wind.speed} speed, ${res[0].wind.gust} gust`;
+  const currentWeatherField = document.createElement("div");
+  currentWeatherField.setAttribute("id", "currentWeatherField");
+  body.appendChild(currentWeatherField);
+  const title = document.createElement("h2");
+  title.innerText = "Current weather for " + res[0].name;
+  currentWeatherField.appendChild(title);
+  const currentWeather = document.createElement("div");
+  currentWeather.classList.add("grid_col_3");
+  currentWeatherField.appendChild(currentWeather);
+  const currentRain = document.createElement("p");
+  currentRain.classList.add("rain");
+  currentWeather.appendChild(currentRain);
+  const currentTemp = document.createElement("p");
+  currentTemp.classList.add("temp");
+  currentWeather.appendChild(currentTemp);
+  const currentWind = document.createElement("p");
+  currentWind.classList.add("wind");
+  currentWeather.appendChild(currentWind);
+  // const windDirection = document.createElement("img");
+  // currentWind.appendChild(windDirection);
+  currentRain.innerText = `${String(
+    res[0].weather.description
+  )[0].toUpperCase()}${String(res[0].weather.description).slice(1)}`;
+  currentRain.insertAdjacentHTML(
+    "afterbegin",
+    `<img src="https://openweathermap.org/img/wn/${res[0].weather.icon}@2x.png" alt="pic" /> <br>`
+  );
+  currentTemp.innerText = `${Math.round(res[0].main.temp)} °C`;
+  currentWind.innerText = `${(res[0].wind.speed * 3.6).toFixed(1)} km/u`;
+}
+
+function printForecast(res) {
+  const body = document.querySelector("body");
+  const forecastField = document.createElement("div");
+  forecastField.setAttribute("id", "forecastField");
+  body.appendChild(forecastField);
+  const title = document.createElement("h2");
+  title.innerText = "5-day forecast for " + res[1].name;
+  forecastField.appendChild(title);
+
+  tempChart(res);
+}
+
+function tempChart(res) {
+  const forecastField = document.querySelector("#forecastField");
+  forecastField.insertAdjacentHTML(
+    "beforeend",
+    `<canvas id="tempChart" style="width:100%;max-width:800px"></canvas>`
+  );
+
+  const dateTimes = Array.from(res[1].list).map((cv) => {
+    return String(cv.dt_txt).slice(11, 13);
+  });
+
+  const temps = Array.from(res[1].list).map((cv) => {
+    return Math.round(cv.main.temp);
+  });
+
+  const xValues = dateTimes;
+  const yValues = temps;
+
+  new Chart("tempChart", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: "orange",
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "5-day temperature forecast for " + res[1].name,
+      },
+    },
+  });
 }
--- basis_js/promises/wordle/script.js
+++ basis_js/promises/wordle/script.js
@@ -3,63 +3,116 @@ const ROUNDS = 6;
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
 
+// the state for the app
+let currentGuess = "";
+let line = 0;
+let wordOTDay;
+let keepGuessing = true;
+
 async function init() {
-  // the state for the app
-  let currentGuess = 0;
-  let currentLetterIndex = 0;
-  let currentLetter;
   // nab the word of the day
-  const wordOTDay = await fetch("https://words.dev-apis.com/word-of-the-day")
+  wordOTDay = await fetch("https://words.dev-apis.com/word-of-the-day")
     .then((res) => res.json())
     .then((res) => res.word);
   console.log(wordOTDay);
+}
 
-  // user adds a letter to the current guess
-  function addLetter(letter) {}
-
-  // use tries to enter a guess
-  async function commit() {}
-
-  // user hits backspace, if the the length of the string is 0 then do
-  // nothing
-  function backspace() {
-    if (currentLetterIndex % 5 === 0) {
-      return;
-    } else {
-      letters[currentLetterIndex - 1].innerHTML = "";
-      currentLetterIndex -= 1;
-    }
-  }
-
-  // let the user know that their guess wasn't a real word
-  // skip this if you're not doing guess validation
-  function markInvalidWord() {}
-
-  // listening for event keys and routing to the right function
-  // we listen on keydown so we can catch Enter and Backspace
-  document.addEventListener("keydown", function handleKeyPress(event) {
-    console.log(event.key);
-    currentLetter = event.key;
+// listening for event keys and routing to the right function
+// we listen on keydown so we can catch Enter and Backspace
+document.addEventListener("keydown", function handleKeyPress(event) {
+  if (keepGuessing === true) {
     if (event.key === "Backspace") {
       backspace();
+    } else if (event.key === "Enter") {
+      commit();
+    } else if (isLetter(event.key)) {
+      addLetter(event.key);
     } else {
-      isLetter();
+      // other keys ignored
+    }
+  }
+});
+
+// user adds a letter to the current guess
+function addLetter(letter) {
+  if (currentGuess.length < ANSWER_LENGTH) {
+    currentGuess += letter;
+  } else {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+  }
+  letters[line * ANSWER_LENGTH + currentGuess.length - 1].innerText = letter;
+}
+
+// use tries to enter a guess
+function commit() {
+  const correctWord = Array.from(wordOTDay);
+  const correctLetters = Array.from(currentGuess).map((letter, i) => {
+    if (letter === correctWord[i]) {
+      return "correct";
+    } else {
+      return letter;
     }
   });
+  const resultsArray = correctLetters.map((letter) => {
+    keepGuessing = false;
+    if (letter === "correct") {
+      return "correct";
+    } else if (correctWord.includes(letter)) {
+      const indexOfLetter = correctWord.indexOf(letter);
+      correctWord[indexOfLetter] = "found";
+      keepGuessing = true;
+      return "close";
+    } else {
+      keepGuessing = true;
+      return "wrong";
+    }
+  });
+
+  for (let i = 0; i < resultsArray.length; i++) {
+    console.log("colored " + resultsArray[i]);
+    letters[line * ANSWER_LENGTH + i].classList.add(resultsArray[i]);
+  }
+
+  if (line < ROUNDS) {
+    line++;
+  } else {
+    keepGuessing = false;
+  }
+
+  currentGuess = "";
+
+  // const res = await fetch("https://words.dev-apis.com/validate-word", {
+  //   method: "POST",
+  //   body: JSON.stringify({ word: currentGuess }),
+  // });
 }
+
+// user hits backspace, if the the length of the string is 0 then do
+// nothing
+function backspace() {
+  currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+  letters[line * ANSWER_LENGTH + currentGuess.length].innerText = "";
+}
+
+// let the user know that their guess wasn't a real word
+// skip this if you're not doing guess validation
+function markInvalidWord() {}
 
 // a little function to check to see if a character is alphabet letter
 // this uses regex (the /[a-zA-Z]/ part) but don't worry about it
 // you can learn that later and don't need it too frequently
-function isLetter(letter) {}
+function isLetter(letter) {
+  re = /^[a-zA-Z]$/;
+  if (re.test(letter)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // show the loading spinner when needed
-function setLoading(isLoading) {}
-
-// takes an array of letters (like ['E', 'L', 'I', 'T', 'E']) and creates
-// an object out of it (like {E: 2, L: 1, T: 1}) so we can use that to
-// make sure we get the correct amount of letters marked close instead
-// of just wrong or correct
-function makeMap(array) {}
+function setLoading(isLoading) {
+  //loadingDiv.classList.toggle("hidden", !isLoading);
+}
 
 init();
